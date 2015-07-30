a179d43f2b35b70e0ea0ed025aca317b813110e1
jsimonelli <jose.simonelli@gmail.com>
2015-07-30 16:25:15 -0400

    added Service Dialogs

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ install_cmd() {
     echo "Importing ${DOMAIN} from ${TOPDIR}/Automate/${DOMAIN}"
     cd /var/www/miq/vmdb
     bin/rake "rhconsulting:miq_ae_datastore:import[${DOMAIN}, ${TOPDIR}/Automate]"
+
+    echo "Importing Service Dialogs"
+    bin/rake rhconsulting:dialogs:import[${DOMAIN}/ServiceDialogs]
 }
 
 if [ -d ${TOPDIR}/Automate/${DOMAIN} ] ; then
